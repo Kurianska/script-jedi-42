fc5b70b8a1ba041a858c9037f07110bac37d8a32
Kurianska <59371877+Kurianska@users.noreply.github.com>
2021-01-13 16:24:01 +0200

    Update day-01.js

--- day-01.js
+++ day-01.js
@@ -1,4 +1,4 @@
-// https://www.codewars.com/kata/571ec274b1c8d4a61c0000c8/train/javascript
+// http://www.codewars.com/kata/571ec274b1c8d4a61c0000c8
 
 function helloWorld () {
   var str = 'Hello World!';
