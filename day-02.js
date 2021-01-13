3a1ba16ce47e6c2ac97e328a93267d2e2a2d0150
Kurianska <59371877+Kurianska@users.noreply.github.com>
2021-01-13 17:46:49 +0200

    Update day-02.js

--- day-02.js
+++ day-02.js
@@ -75,3 +75,23 @@ function answer3() {
 
 // http://www.codewars.com/kata/571effabb625ed9b0600107a
 
+function getLength(arr) {
+  return arr.length;
+}
+function getFirst(arr) {
+  return arr[0];
+}
+function getLast(arr) {
+  return arr[arr.length - 1];
+}
+function pushElement(arr) {
+  arr.push(1);
+  return arr;
+}
+function popElement(arr) {
+  arr.pop();
+  return arr;
+}
+
+// http://www.codewars.com/kata/571f1eb77e8954a812000837
+
