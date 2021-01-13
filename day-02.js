b3eaa7fd482506787003847f43e8f04dc181394f
Kurianska <59371877+Kurianska@users.noreply.github.com>
2021-01-13 16:23:00 +0200

    Update day-02.js

--- day-02.js
+++ day-02.js
@@ -1,1 +1,42 @@
+// https://www.codewars.com/kata/571edd157e8954bab500032d
 
+var v1 = 50,
+  v2 = 100,
+  v3 = 150,
+  v4 = 200,
+  v5 = 2,
+  v6 = 250;
+function equal1() {
+  var a = v1;
+  var b = v1;
+  return a + b;
+}
+
+function equal2() {
+  let a = v3;
+  let b = v1;
+  return a - b;
+}
+function equal3() {
+  let a = v1;
+  let b = v5;
+  return a * b;
+}
+function equal4() {
+  let a = v4;
+  let b = v5;
+  return a / b;
+}
+function equal5() {
+  let a = v6;
+  let b = v3;
+  return a % b;
+}
+
+equal1();
+equal2();
+equal3();
+equal4();
+
+
+// http://www.codewars.com/kata/571edea4b625edcb51000d8e
