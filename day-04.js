2ad048fa0d1aac20316eec17b29c8284d91ed36a
Kurianska <59371877+Kurianska@users.noreply.github.com>
2021-01-17 16:38:44 +0200

    Update day-04.js

--- day-04.js
+++ day-04.js
@@ -1,1 +1,10 @@
+// https://www.codewars.com/kata/5721a78c283129e416000999
 
+function pickIt(arr) {
+  let odd = [],
+    even = [];
+  for (let i = 0; i < arr.length; i++) {
+    arr[i] % 2 === 0 ? even.push(arr[i]) : odd.push(arr[i]);
+  }
+  return [odd, even];
+}
