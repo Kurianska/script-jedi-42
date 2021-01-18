4b711c7a4e580332b9c27cde14fa357532d16394
Kurianska <59371877+Kurianska@users.noreply.github.com>
2021-01-18 11:54:53 +0200

    Update day-04.js

--- day-04.js
+++ day-04.js
@@ -1,3 +1,15 @@
+// http://www.codewars.com/kata/57216d4bcdd71175d6000560
+
+function padIt(str, n) {
+  let count = 0;
+  while (count < n) {
+    count % 2 === 0 ? (str = "*" + str) : (str += "*");
+    count++;
+  }
+  return str;
+}
+
+
 // https://www.codewars.com/kata/5721a78c283129e416000999
 
 function pickIt(arr) {
