7edb422576f2fbd323ecf73c34b53c23b1700910
Kurianska <59371877+Kurianska@users.noreply.github.com>
2021-01-21 15:15:55 +0200

    Update day-05.js

--- day-05.js
+++ day-05.js
@@ -4,3 +4,12 @@ function correctTail(body, tail) {
   sub = body.substr(body.length - tail.length);
   return sub === tail ? true : false;
 }
+
+// https://www.codewars.com/kata/56f6ad906b88de513f000d96
+
+function bonusTime(salary, bonus) {
+  return "£" + (bonus ? salary * 10 : salary);
+}
+
+// https://www.codewars.com/kata/5a58d889880385c2f40000aa
+
