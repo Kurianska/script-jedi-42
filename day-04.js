c8a6ce52d0da7ba013a43ea6787fc6a00ce904a6
Kurianska <59371877+Kurianska@users.noreply.github.com>
2021-01-23 18:58:37 +0200

    Update day-04.js

--- day-04.js
+++ day-04.js
@@ -39,4 +39,15 @@ function grabDoll(dolls) {
 
 // http://www.codewars.com/kata/5722b3f0bd5583cf44001000
 
-
+function giveMeFive(obj) {
+  let arr = [];
+  for (let key in obj) {
+    if (key.length === 5) {
+      arr.push(key);
+    }
+    if (obj[key].length === 5) {
+      arr.push(obj[key]);
+    }
+  }
+  return arr;
+}
