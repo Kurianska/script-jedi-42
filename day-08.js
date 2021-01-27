ebba692c6b9c988819818af7c896c744e4b9ee9b
Kurianska <59371877+Kurianska@users.noreply.github.com>
2021-01-27 16:40:19 +0200

    Update day-08.js

--- day-08.js
+++ day-08.js
@@ -26,3 +26,14 @@ function whatNumberIsIt(n) {
 
 
 // https://www.codewars.com/kata/57238ceaef9008adc7000603
+
+function colorOf(r,g,b){
+  r.toString(16).length < 2 ? r = '0' + r.toString(16) : r = r.toString(16);
+  g.toString(16).length < 2 ? g = '0' + g.toString(16) : g = g.toString(16);
+  b.toString(16).length < 2 ? b = '0' + b.toString(16) : b = b.toString(16);
+  
+  return '#' + r + g + b;
+}
+
+// http://www.codewars.com/kata/57256064856584bc47000611
+
