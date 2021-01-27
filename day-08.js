629034807c0b8d16b4f2cc028118914f420d27a9
Kurianska <59371877+Kurianska@users.noreply.github.com>
2021-01-27 12:12:24 +0200

    Update day-08.js

--- day-08.js
+++ day-08.js
@@ -1,1 +1,28 @@
+// http://www.codewars.com/kata/5722fd3ab7162a3a4500031f
 
+function whatNumberIsIt(n) {
+  let str;
+  switch (!NaN && n) {
+    case Number.MAX_VALUE:
+      str = 'Input number is Number.MAX_VALUE';
+      break;
+    case Number.MIN_VALUE:
+      str = 'Input number is Number.MIN_VALUE';
+      break;
+    case false:
+      str = 'Input number is Number.NaN';
+
+    case -Infinity:
+      str = 'Input number is Number.NEGATIVE_INFINITY';
+      break;
+    case Infinity:
+      str = 'Input number is Number.POSITIVE_INFINITY';
+      break;
+    default:
+      str = 'Input number is ' + n;
+  }
+  return str;
+}
+
+
+// https://www.codewars.com/kata/57238ceaef9008adc7000603
