81161175c584a8050dfd59daaab0bad2b31e7de2
Kurianska <59371877+Kurianska@users.noreply.github.com>
2021-02-06 17:28:23 +0200

    Update day-05.js

--- day-05.js
+++ day-05.js
@@ -1,7 +1,7 @@
 // https://www.codewars.com/kata/is-this-my-tail/train/javascript
 
 function correctTail(body, tail) {
-  sub = body.substr(body.length - tail.length);
+  let sub = body.substr(body.length - tail.length);
   return sub === tail ? true : false;
 }
 
