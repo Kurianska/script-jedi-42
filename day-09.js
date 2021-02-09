2388457eca6b6acb56891ba090cc6c5c8a6daae6
Kurianska <59371877+Kurianska@users.noreply.github.com>
2021-02-09 18:08:43 +0200

    Update day-09.js

--- day-09.js
+++ day-09.js
@@ -13,3 +13,5 @@ function cutIt(arr) {
 function firstToLast(str,c){
   return str.includes(c) ? str.lastIndexOf(c) - str.indexOf(c) : -1;
 }
+
+// http://www.codewars.com/kata/57280481e8118511f7000ffa
