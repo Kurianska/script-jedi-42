368aa5323407128afbbd6d61a7df7b92debef693
Kurianska <59371877+Kurianska@users.noreply.github.com>
2021-02-09 16:47:35 +0200

    Update day-09.js

--- day-09.js
+++ day-09.js
@@ -1,9 +1,9 @@
 // http://www.codewars.com/kata/57274562c8dcebe77e001012
 
 function cutIt(arr) {
-  const itemMin = arr.reduce(function (shortest, currentWord) {
-    return currentWord.length < shortest.length ? currentWord : shortest;
-  });
+  const itemMin = arr.reduce((shortest, currentWord) =>
+    currentWord.length < shortest.length ? currentWord : shortest
+  );
   const result = arr.map((item) => item.slice(0, itemMin.length));
   return result;
 }
