393c1bafd2afce9041a01ec25fa4b850c70bcb82
Kurianska <59371877+Kurianska@users.noreply.github.com>
2021-02-11 20:33:31 +0200

    Update day-11.js

--- day-11.js
+++ day-11.js
@@ -190,6 +190,34 @@ describe('`Set` API overview', function(){
 
 // http://tddbin.com/#?kata=es6/language/set/clear
 
+// 70: Set - clear
+// To do: make all tests pass, leave the assert lines unchanged!
+// Follow the hints of the failure messages!
 
+describe('`clear()` removes all elements from a Set object.', function(){
+  let set;
+  beforeEach(() => set = new Set());
+  it('`set.size` becomes 0', function() {
+    set.add('one').add(2);
+    set.clear();
+    var actualSize = 0;
+    assert.equal(actualSize, set.size);
+  });
+  it('the iterator `set.entries()` will not contain any items', function() {
+    set.add('one').add(2);
+    set.clear();
+    const {done} = set.entries().next();
+    assert.equal(done, true);
+  });
+  it('any call to `set.has()` returns false', function() {
+    set.add('one').add('two');
+    
+    assert.deepEqual(set.has(2), false);
+  });
+  it('returns `undefined`', function() {
+    var actualReturn = undefined;
+    assert.equal(actualReturn, set.clear());
+  });
+});
 
 
