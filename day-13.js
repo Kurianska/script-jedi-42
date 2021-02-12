e9a8443854ce26c49725c8923cc5ced5f9f15659
Kurianska <59371877+Kurianska@users.noreply.github.com>
2021-02-12 15:06:14 +0200

    Update day-13.js

--- day-13.js
+++ day-13.js
@@ -39,3 +39,44 @@ describe('`Map` is a key/value map', function(){
 });
 
 // http://tddbin.com/#?kata=es6/language/map/get
+
+// 45: Map.prototype.get()
+// To do: make all tests pass, leave the assert lines unchanged!
+// Follow the hints of the failure messages!
+
+describe('`Map.prototype.get` returns the element from the map for a key', function(){
+  it('`get(key)` returns the value stored for this key', function() {
+    let map = new Map();
+    map.set('key', 'value');
+    const value = map.get('key');
+    assert.equal(value, 'value');
+  });
+  it('multiple calls still return the same value', function() {
+    let map = new Map();
+    map.set('key', 'value');
+    map.set('key', 'value');
+    var value = map.get('key');
+    assert.equal(value, 'value');
+  });
+  it('requires exactly the value as passed to `set()`', function() {
+    let map = new Map();
+    const obj = {};
+    map.set(obj, 'object is key');
+    assert.equal(map.get(obj), 'object is key');
+  });
+  it('leave out the key, and you get the value set for the key `undefined` (void 0)', function() {
+    let map = new Map();
+    map.set(1, 'yo');
+    const value = map.get(1);
+    assert.equal(value, 'yo');
+  });
+  it('returns undefined for an unknown key', function() {
+    let map = new Map();
+    map.set(void 0, 1);
+    const value = map.get('1');
+    assert.equal(value, void 0);
+  });
+});
+
+
+// http://tddbin.com/#?kata=es6/language/map/set
