9ba77b7e8ca4d9fb8c68b3f7b029a989376f5306
Kurianska <59371877+Kurianska@users.noreply.github.com>
2021-02-15 13:42:44 +0200

    Update day-12.js

--- day-12.js
+++ day-12.js
@@ -1,1 +1,15 @@
+// https://www.codewars.com/kata/playing-with-sets-equal-or-not/train/javascript
 
+function areEqual(s1, s2) {
+  const set1 = new Set(s1);
+  const set2 = new Set(s2);
+  return (s1, s2) =>
+    set1.size === set2.size && [...set1].every((value) => set2.has(value));
+}
+
+function notEqual(s1, s2) {
+  const set1 = new Set(s1);
+  const set2 = new Set(s2);
+  return (s1, s2) =>
+    set1.size !== set2.size || ![...set1].every((value) => set2.has(value));
+}
