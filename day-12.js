f8914a1012d62d61e7af2bc4ef0fc8184170befa
Kurianska <59371877+Kurianska@users.noreply.github.com>
2021-02-15 22:38:57 +0200

    Update day-12.js

--- day-12.js
+++ day-12.js
@@ -8,8 +8,5 @@ function areEqual(s1, s2) {
 }
 
 function notEqual(s1, s2) {
-  const set1 = new Set(s1);
-  const set2 = new Set(s2);
-  return (s1, s2) =>
-    set1.size !== set2.size || ![...set1].every((value) => set2.has(value));
+  return !areEqual(s1, s2)
 }
