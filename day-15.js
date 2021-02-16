70d25d00676d5d33e69d49004ba8064f59011107
Kurianska <59371877+Kurianska@users.noreply.github.com>
2021-02-16 12:07:21 +0200

    Update day-15.js

--- day-15.js
+++ day-15.js
@@ -1,1 +1,12 @@
+// http://www.codewars.com/kata/572ab0cfa3af384df7000ff8
+
+const shuffleIt = (arr, ...numArr) => {
+    for (let i=0; i < numArr.length; i++) {
+      [arr[numArr[i][0]], arr[numArr[i][1]]] = [arr[numArr[i][1]], arr[numArr[i][0]]];
+    }
+    return arr;
+}
+
+
+// https://www.codewars.com/kata/5fd8aa5743b49e0012d43e50/
 
