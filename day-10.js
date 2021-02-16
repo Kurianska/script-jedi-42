8c68de3915057969e68f217ff4f2fd261e00fa10
Kurianska <59371877+Kurianska@users.noreply.github.com>
2021-02-16 20:52:36 +0200

    Update day-10.js

--- day-10.js
+++ day-10.js
@@ -7,14 +7,14 @@ function alienLanguage(str) {
 
 // http://www.codewars.com/kata/57284d23e81185ae6200162a
 
-function topSecret(str) {
-  let s = str
-    .split('')
-    .map((char) => String.fromCharCode(char.charCodeAt() - 3))
-    .join('');
-  s = s.match(/[a-z]/gi).join('');
-  return s;
-}
+const topSecret = str => str.replace(/[a-z]/gi, c => String.fromCharCode(c.charCodeAt(0) + (/[abc]/i.test(c) ? 23 : -3)))
+
+//question1: The top secret file number is...
+answer1="2912";
+//question2: Super agent's name is...
+answer2="WsNSq";
+//question3: He stole the treasure is...
+answer3="Apple core";
 
 // http://www.codewars.com/kata/5729b103dd8bac11a900119e
 
