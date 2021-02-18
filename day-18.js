40c9694ac5aabcf9615847ad1b2fa429ecfc4f63
Kurianska <59371877+Kurianska@users.noreply.github.com>
2021-02-18 15:26:18 +0200

    Update day-18.js

--- day-18.js
+++ day-18.js
@@ -116,4 +116,69 @@ describe('`Array.prototype.find` makes finding items in arrays easier', () => {
 
 // http://tddbin.com/#?kata=es6/language/array-api/findIndex
 
+// 33: array - `Array.prototype.findIndex` 
+// To do: make all tests pass, leave the assert lines unchanged!
+// Follow the hints of the failure messages!
 
+describe('`Array.prototype.findIndex` makes finding items in arrays easier', () => {
+  it('takes a compare function, returns the index where it returned true', function() {
+    const foundAt = [false, true].findIndex(item => item === true);
+    assert.equal(foundAt, 1);
+  });
+  it('returns the first position it was found at', function() {
+    const foundAt = [0, 1, 1, 1].findIndex(item => item === 1);
+    assert.equal(foundAt, 1);
+  });
+  it('returns `-1` when nothing was found', function() {
+    const foundAt = [1, 2, 3].findIndex(item => item < 1);
+    assert.equal(foundAt, -1);
+  });
+  it('the findIndex callback gets the item, index and array as arguments', function() {
+    const three = 3;
+    const containsThree = arr => arr.indexOf(three) > -1;
+    function theSecondThree(item, index, arr) {
+      const preceedingItems = arr.slice(0, index);
+      return containsThree(preceedingItems);
+    }
+    const foundAt = [1, 1, 2, 3, 3, 3].findIndex(theSecondThree);
+    assert.equal(foundAt, 4);
+  });
+  it('combined with destructuring complex compares become short', function() {
+    const bob = {name: 'Bob'};
+    const alice = {name: 'Alice'};
+    const foundAt = [bob, alice].findIndex(({name: value}) => value.length > 3);
+    assert.equal(foundAt, 1);
+  });
+});
+
+// http://tddbin.com/#?kata=es6/language/array-api/entries
+
+// 41: array - entries
+// To do: make all tests pass, leave the assert lines unchanged!
+// Follow the hints of the failure messages!
+
+describe('`[].entries()` returns an iterator object with all entries', function() {
+  it('returns key+value for each element', function() {
+    const arr = ['a', 'b', 'c'];
+    const entriesAsArray = Array.from(arr.entries());
+    assert.deepEqual(entriesAsArray, [[0,"a"], [1,"b"], [2,"c"]]);
+  });
+  it('empty elements contain the value `undefined`', function() {
+    const arr = ['one'];
+    arr[2] = 'three';
+    const secondValue = Array.from(arr.entries())[1];
+    assert.deepEqual(secondValue, [1, void 0]);
+  });
+  describe('returns an iterable', function() {
+    it('has `next()` to iterate', function() {
+      const arr = ['one'];
+      const value = arr.entries().next().value
+       assert.deepEqual(value, [0, 'one']);
+    });
+  });
+});
+
+
+// http://tddbin.com/#?kata=es6/language/array-api/keys
+
+// http://tddbin.com/#?kata=es6/language/array-api/values
