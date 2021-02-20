d381e11e5c88783f391fdcde871d33382ff5dc9f
Kurianska <59371877+Kurianska@users.noreply.github.com>
2021-02-21 00:00:32 +0200

    Update day-16.js

--- day-16.js
+++ day-16.js
@@ -24,5 +24,26 @@ function isolateIt(arr) {
 
 // http://www.codewars.com/kata/573023c81add650b84000429
 
+function countGrade(scores) {
+  const obj = {
+    S: scores.filter((x) => x === 100).length,
+    A: scores.filter((x) => x < 100 && x >= 90).length,
+    B: scores.filter((x) => x < 90 && x >= 80).length,
+    C: scores.filter((x) => x < 80 && x >= 60).length,
+    D: scores.filter((x) => x < 60 && x >= 0).length,
+    X: scores.filter((x) => x == -1).length,
+  };
+  return obj;
+}
 
 // http://www.codewars.com/kata/57308546bd9f0987c2000d07
+
+function mirrorImage(arr) {
+  for (let i = 0; i < arr.length - 1; i++) {
+    if (
+      arr[i].toString().split("").reverse().join("") === arr[i + 1].toString()
+    )
+      return [arr[i], arr[i + 1]];
+  }
+  return [-1, -1];
+}
