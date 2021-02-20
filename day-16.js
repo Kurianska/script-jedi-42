df47389c70a9216b40c2a0ca7e2bf3b1f605f1ee
Kurianska <59371877+Kurianska@users.noreply.github.com>
2021-02-20 19:39:24 +0200

    Update day-16.js

--- day-16.js
+++ day-16.js
@@ -1,23 +1,25 @@
 // http://www.codewars.com/kata/572df796914b5ba27c000c90
 
 function sortIt(arr) {
-  return arr.slice().sort((a,b)=>{
-  let itemA = arr.filter(i => i === a).length;
-  let itemB = arr.filter(i => i === b).length;
+  const myArr = arr.slice().sort((a,b)=>{
+  const itemA = arr.filter(i => i === a).length;
+  const itemB = arr.filter(i => i === b).length;
   if (itemA === itemB) return b-a;
   if (itemA > itemB) return 1;
   return -1;
   })
+  return myArr;
 }
 
 // http://www.codewars.com/kata/572fdeb4380bb703fc00002c
 
 function isolateIt(arr) {
-  return arr.map((i) => {
+  const myArr = arr.map((i) => {
     return i.length % 2 === 0
       ? i.slice(0, i.length / 2) + "|" + i.slice(i.length / 2)
       : i.slice(0, i.length / 2) + "|" + i.slice(i.length / 2 + 1);
   });
+  return myArr;
 }
 
 // http://www.codewars.com/kata/573023c81add650b84000429
