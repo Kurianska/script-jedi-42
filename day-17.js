34dd319ad538309bdf2adccd6b9271e553a5ec35
Kurianska <59371877+Kurianska@users.noreply.github.com>
2021-02-21 21:48:21 +0200

    Update day-17.js

--- day-17.js
+++ day-17.js
@@ -10,7 +10,7 @@
 function tailAndHead(arr) {
   const newArr = [];
   for (let i = 0; i < arr.length - 1; i++)
-    newArr.push((arr[i] % 10) + Number((arr[i + 1] + "")[0]));
+    newArr.push((arr[i] % 10) + Number((arr[i + 1] + '')[0]));
   return newArr.reduce((a, b) => a * b);
 }
 
@@ -20,8 +20,8 @@ function blackAndWhite(arr) {
   return !Array.isArray(arr)
     ? "It's a fake array"
     : arr.includes(5) && arr.includes(13)
-    ? "It's a black array"
-    : "It's a white array";
+    ? 'It's a black array'
+    : 'It's a white array';
 }
 
 // https://www.codewars.com/kata/array-number-reduce/train/javascript
