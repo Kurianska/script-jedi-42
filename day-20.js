0e19595a3caf11043cff9db5223cd73c2bcda8e0
Kurianska <59371877+Kurianska@users.noreply.github.com>
2021-02-21 22:09:04 +0200

    Create day-20.js

---
+++ day-20.js
@@ -0,0 +1,14 @@
+// http://www.codewars.com/kata/5735e39313c205fe39001173
+
+function countAnimals(animals, count) {
+  let result = [];
+  for (let i of count) {
+    result.push(animals.split(i).length - 1);
+  }
+  return result;
+}
+
+// http://www.codewars.com/kata/573975d3ac3eec695b0013e0
+
+
+// https://www.codewars.com/kata/573bca07dffc1aa693000139
