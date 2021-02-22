5b077bf890880a223d88ff134525b7b3d94bb50f
Kurianska <59371877+Kurianska@users.noreply.github.com>
2021-02-22 12:14:33 +0200

    Update day-17.js

--- day-17.js
+++ day-17.js
@@ -26,5 +26,15 @@ function blackAndWhite(arr) {
 
 // https://www.codewars.com/kata/array-number-reduce/train/javascript
 
+Array.prototype.reduce = function(process, initial) {
+  for (let i = 0; i < this.length; i++) {
+    if (!initial) {
+      i++; initial=this[0]
+    }
+    initial =  process(initial, this[i]);
+ }
+ return initial
+}
+
 
 
