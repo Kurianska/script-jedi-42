d6dac8d8f8f44c35d8d86ee38155a860ea81433a
Kurianska <59371877+Kurianska@users.noreply.github.com>
2021-02-22 12:34:34 +0200

    Update day-21.js

--- day-21.js
+++ day-21.js
@@ -1,12 +1,12 @@
 // http://www.codewars.com/kata/573d11c48b97c0ad970002d4
 
-let regex=/(http|https):\/\/[a-z\d.]+\.(net|com)/gi;
+const regex=/(http|https):\/\/[a-z\d.]+\.(net|com)/gi;
 
 // http://www.codewars.com/kata/573e6831e3201f6a9b000971
 
 // http://www.codewars.com/kata/573fb9223f9793e485000453
 
-let regex = /(\d)(?=(\d{3})+$)/g;
+const regex = /(\d)(?=(\d{3})+$)/g;
 function addCommas(money, reg) {
   return money.replace(reg, (x) => x + ',');
 }
