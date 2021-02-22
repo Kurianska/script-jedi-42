19d7bb2d2934d540d04549d14202a71818595718
Kurianska <59371877+Kurianska@users.noreply.github.com>
2021-02-22 17:31:22 +0200

    Update day-20.js

--- day-20.js
+++ day-20.js
@@ -10,6 +10,15 @@ function countAnimals(animals, count) {
 
 // http://www.codewars.com/kata/573975d3ac3eec695b0013e0
 
+function findSimilarity(str, word) {
+  const regstr =
+    word[0] + word.slice(1, -1).replace(/./g, '.') + word.slice(-1); 
+  const reg1 = new RegExp('^' + regstr + '$');
+  return str
+    .split(' ')
+    .filter(v => reg1.test(v))
+    .join(' ');
+}
 
 // https://www.codewars.com/kata/573bca07dffc1aa693000139
 
