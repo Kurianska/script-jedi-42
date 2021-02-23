1243b26fc68ff3c12687059973fdb756ae114dff
Kurianska <59371877+Kurianska@users.noreply.github.com>
2021-02-23 17:05:38 +0200

    Update day-23.js

--- day-23.js
+++ day-23.js
@@ -1,1 +1,11 @@
+// http://www.codewars.com/kata/unpacking-arguments
 
+function spread(func, args) {
+  return func(...args);
+}
+
+// http://www.codewars.com/kata/for-the-sake-of-argument
+
+function numbers(...args) {
+  return [...args].every((arg) => typeof arg === 'number');
+}
