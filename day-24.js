0ad815fde547feb4bc79eb28ad0cb2f500066857
Kurianska <59371877+Kurianska@users.noreply.github.com>
2021-02-24 09:29:53 +0200

    Update day-24.js

--- day-24.js
+++ day-24.js
@@ -1,16 +1,13 @@
 // http://www.codewars.com/kata/duplicate-arguments
 
 function solution(...args) {
-   return args.length !== new Set(args).size;
-}
-
-function solution(...args) {
   return [...args].sort().some((cur, i, arr) => cur === arr[i + 1]);
 }
 
 
 //  http://www.codewars.com/kata/last
 
-function last(list) {
-  const last = arguments[arguments.length - 1]
-  return last[last.length - 1] || last
+function last(...args){
+const last = args[args.length - 1];
+  return last[last.length - 1] || last;
+}
