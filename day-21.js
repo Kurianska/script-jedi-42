260fca55f6bc1be105c297f95cb324dd1f1158e8
Kurianska <59371877+Kurianska@users.noreply.github.com>
2021-02-24 09:42:28 +0200

    Update day-21.js

--- day-21.js
+++ day-21.js
@@ -4,6 +4,8 @@ const regex=/(http|https):\/\/[a-z\d.]+\.(net|com)/gi;
 
 // http://www.codewars.com/kata/573e6831e3201f6a9b000971
 
+const regex=/\b(\w)(\w)?(\w)?\w?\3\2\1\b/g;
+
 // http://www.codewars.com/kata/573fb9223f9793e485000453
 
 const regex = /(\d)(?=(\d{3})+$)/g;
