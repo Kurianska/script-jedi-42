b14498ad0d409d39517e3dfc84b6b8fa8ba33de4
Kurianska <59371877+Kurianska@users.noreply.github.com>
2021-02-26 16:52:54 +0200

    Update day-25.js

--- day-25.js
+++ day-25.js
@@ -1,1 +1,18 @@
+//  http://www.codewars.com/kata/write-javascripts-call-function-using-apply
 
+Function.prototype.call = function(context, ...args) {
+  return this.apply(context, args);
+};
+
+
+//  http://www.codewars.com/kata/anonymous-returns
+
+const name = 'The Window';
+const alpha = {
+  name: 'My Alpha',
+  getNameFunc: function () {
+    return function () {
+      return this.name;
+    }.bind(this);
+  },
+};
