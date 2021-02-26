bdfbe4366fc51801bf31e45cd4d91d3ff101bb3f
Kurianska <59371877+Kurianska@users.noreply.github.com>
2021-02-26 17:34:35 +0200

    Update day-26.js

--- day-26.js
+++ day-26.js
@@ -1,3 +1,11 @@
 //  http://www.codewars.com/kata/power-bind
 
+Function.prototype.bind = function (ctx) {
+   return (childCxt = ctx) => this.call(childCxt, childCxt);
+};
+
 // https://www.codewars.com/kata/sort-with-arrow-functions
+
+const OrderPeople = function(people){
+  return people.sort((a, b) => a.age-b.age);
+}
