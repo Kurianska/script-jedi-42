b3e81e830792b1ea265bbb4d4d996a5ad95eb772
Kurianska <59371877+Kurianska@users.noreply.github.com>
2021-02-27 23:13:05 +0200

    Update day-27.js

--- day-27.js
+++ day-27.js
@@ -12,3 +12,12 @@ function buildFun(n) {
 
 // https://www.codewars.com/kata/shifty-closures/train/javascript
 
+const greet_abe = function () {
+  const name = 'Abe';
+  return 'Hello, ' + name + '!';
+};
+const greet_ben = function () {
+  const name = 'Ben';
+  return 'Hello, ' + name + '!';
+};
+
