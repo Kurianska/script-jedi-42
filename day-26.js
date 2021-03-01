8a13a39b4c3c91c088df45aa77496fbb4cfa8132
Kurianska <59371877+Kurianska@users.noreply.github.com>
2021-03-01 18:05:39 +0200

    Update day-26.js

--- day-26.js
+++ day-26.js
@@ -1,7 +1,11 @@
 //  http://www.codewars.com/kata/power-bind
 
 Function.prototype.bind = function (ctx) {
-   return (childCxt = ctx) => this.call(childCxt, childCxt);
+  const func = this;
+  return function (...arg) {
+    const rigthCtx = this === global ? ctx : this;
+    return func.apply(rigthCtx, arg);
+  };
 };
 
 // https://www.codewars.com/kata/sort-with-arrow-functions
