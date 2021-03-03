10c525b3071b7a38718b2173dccac51c21ef896b
Kurianska <59371877+Kurianska@users.noreply.github.com>
2021-03-03 18:51:37 +0200

    Update day-29.js

--- day-29.js
+++ day-29.js
@@ -5,3 +5,11 @@ myObj.__proto__ = MyObject.prototype;
 MyObject.call(myObj);
 
 // http://www.codewars.com/kata/replicate-new
+
+function nouveau(Constructor, ...args) {
+  const instance = Object.create(Constructor.prototype);
+  const result = Constructor.apply(instance, args);
+  return result && (typeof result === 'object' || typeof result === 'function')
+    ? result
+    : instance;
+}
