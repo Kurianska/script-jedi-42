f704be6f4dfc53bb94d486ece9dbbbfbc3239d11
Kurianska <59371877+Kurianska@users.noreply.github.com>
2021-03-04 20:33:01 +0200

    Create day-32.js

---
+++ day-32.js
@@ -0,0 +1,9 @@
+// http://www.codewars.com/kata/this-is-a-problem
+
+function NameMe(first, last) {
+    this.firstName = first;
+    this.lastName = last;
+  return this.name = `${first} ${last}`;
+}
+
+// https://www.codewars.com/kata/this-is-an-other-problem
