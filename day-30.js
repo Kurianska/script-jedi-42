6c364e2eea2edfa796a26c88392ee12126f7a29e
Kurianska <59371877+Kurianska@users.noreply.github.com>
2021-03-04 18:43:44 +0200

    Update day-30.js

--- day-30.js
+++ day-30.js
@@ -4,5 +4,25 @@ function isSantaClausable(obj) {
  return ['sayHoHoHo', 'distributeGifts', 'goDownTheChimney'].every(method => typeof(obj[method]) ==='function');
 }
 
-// 
+// http://www.codewars.com/kata/cylon-evolution
+
+class Cylon {
+  constructor(model) {
+    this.model = model;
+  }
+  attack() {
+    return "Destroy all humans!";
+  }
+}
+class HumanSkin extends Cylon {
+  constructor(model) {
+    super(model);
+  }
+  infiltrate() {
+    return "Infiltrate the colonies";
+  }
+}
+
+// https://www.codewars.com/kata/588a00ad70720f2cd9000005/train/javascript
+
 
