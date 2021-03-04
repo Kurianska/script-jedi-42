80af3701a9dc1bc2b656256b0df6ccfe43383b5c
Kurianska <59371877+Kurianska@users.noreply.github.com>
2021-03-04 15:42:30 +0200

    Update day-30.js

--- day-30.js
+++ day-30.js
@@ -1,1 +1,8 @@
+// http://www.codewars.com/kata/santaclausable-interface
+
+function isSantaClausable(obj) {
+ return ['sayHoHoHo', 'distributeGifts', 'goDownTheChimney'].every(method => typeof(obj[method]) ==='function');
+}
+
+// 
 
