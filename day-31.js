3a5ea17464b5d2adc5e01407357869e5530a385b
Kurianska <59371877+Kurianska@users.noreply.github.com>
2021-03-05 17:52:08 +0200

    Update day-31.js

--- day-31.js
+++ day-31.js
@@ -34,7 +34,7 @@ describe('The object literal allows for new shorthands', () => {
       assert.deepEqual(short.inlineFunc(), 'I am inline');
     });
     
-    
+   
 // http://tddbin.com/#?kata=es6/language/object-literal/computed-properties
 
     // 16: object-literal - computed properties
@@ -252,5 +252,46 @@ describe('Class accessors (getter and setter)', () => {
 });
 
   // http://tddbin.com/#?kata=es6/language/class/static
+  
+  // 24: class - static keyword
+// To do: make all tests pass, leave the assert lines unchanged!
+// Follow the hints of the failure messages!
+
+describe('Inside a class you can use the `static` keyword', () => {
+  describe('for methods', () => {
+    class UnitTest {}
+    it('a static method just has the prefix `static`', () => {
+      class TestFactory {
+        static makeTest() { return new UnitTest(); }
+      }
+      assert.ok(TestFactory.makeTest() instanceof UnitTest);
+    });
+    it('the method name can be dynamic/computed at runtime', () => {
+      const methodName = 'createTest';
+      class TestFactory {
+        static [methodName]() { return new UnitTest(); }
+      }
+      assert.ok(TestFactory.createTest() instanceof UnitTest);
+    });
+  });
+  describe('for accessors', () => {
+    it('a getter name can be static, just prefix it with `static`', () => {
+      class UnitTest {
+         static get testType() { return 'unit'; }
+      }
+      assert.equal(UnitTest.testType, 'unit');
+    });
+    it('even a static getter name can be dynamic/computed at runtime', () => {
+      const type = 'test' + 'Type';
+      class IntegrationTest {
+        static get [type]() { return 'integration'; }
+      }
+      assert.ok('testType' in IntegrationTest);
+      assert.equal(IntegrationTest.testType, 'integration');
+    });
+  });
+});
+
+  // http://tddbin.com/#?kata=es6/language/class/extends
   
   
