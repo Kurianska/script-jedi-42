5b121d0cd2bf90a3f546b3f55a3f51a2a0ae3dfb
Kurianska <59371877+Kurianska@users.noreply.github.com>
2021-03-07 19:52:26 +0200

    Update day-32.js

--- day-32.js
+++ day-32.js
@@ -7,3 +7,20 @@ function NameMe(first, last) {
 }
 
 // https://www.codewars.com/kata/this-is-an-other-problem
+
+function NamedOne(first, last) {
+  return {
+    firstName: first,
+    lastName: last,
+    get fullName() {
+      return `${this.firstName} ${this.lastName}`;
+    },
+    set fullName(name) {
+      const words = name.toString().split(" ");
+      if (words[0] && words[1]) {
+        this.firstName = words[0];
+        this.lastName = words[1];
+      }
+    },
+  };
+}
