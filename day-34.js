b91d5a387cee70d09577431051a26f848ed7c4cc
Kurianska <59371877+Kurianska@users.noreply.github.com>
2021-03-08 16:47:12 +0200

    Update day-34.js

--- day-34.js
+++ day-34.js
@@ -11,5 +11,11 @@ class Woman extends Human {}
 
 // https://www.codewars.com/kata/56ff9b53140fcca90b000530/
 
+class Labrador extends Dog {
+  constructor(name, age, gender, master) {
+    super(name, age, gender, 'Labrador', 'Large', master, true)
+  }
+}
+
 
 // https://www.codewars.com/kata/55a144eff5124e546400005a
