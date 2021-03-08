16f2a4f4335cd7208848ffb5f4b34ede64d239fe
Kurianska <59371877+Kurianska@users.noreply.github.com>
2021-03-08 15:53:39 +0200

    Create day-34.js

---
+++ day-34.js
@@ -0,0 +1,10 @@
+// https://www.codewars.com/kata/basic-subclasses-adam-and-eve/
+
+class God {
+  static create() {
+    return [new Man(), new Woman()];
+  }
+}
+class Human {}
+class Man extends Human {}
+class Woman extends Human {}
