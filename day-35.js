10068bdeab16de6837b3e57bbb924a44e1f7babd
Kurianska <59371877+Kurianska@users.noreply.github.com>
2021-03-09 12:51:31 +0200

    Update day-35.js

--- day-35.js
+++ day-35.js
@@ -10,3 +10,13 @@ thingamabob.__proto__ = Object.create(obj);
 
 
 // https://www.codewars.com/kata/javascript-class-like-objects
+
+class Animal {
+  constructor(name, type) {
+    this.name = name;
+    this.type = type;
+  }
+  toString() {
+    return `${this.name} is a ${this.type}`;
+  }
+}
