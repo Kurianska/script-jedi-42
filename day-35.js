d47d8103e92862f450850507416dbc62c1c3ece5
Kurianska <59371877+Kurianska@users.noreply.github.com>
2021-03-09 11:52:30 +0200

    Update day-35.js

--- day-35.js
+++ day-35.js
@@ -1,1 +1,12 @@
+// https://www.codewars.com/kata/js-prototypes-module-number-1-object-prototypes
 
+const obj = {
+  description: 'TODO: GIVE ME A DESCRIPTION',
+};
+
+widget.__proto__ = Object.create(obj);
+gadget.__proto__ = Object.create(obj);
+thingamabob.__proto__ = Object.create(obj);
+
+
+// https://www.codewars.com/kata/javascript-class-like-objects
