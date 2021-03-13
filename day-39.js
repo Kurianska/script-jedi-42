ceb65fecf6b2af853162ed8879ba15c04988f4dd
Kurianska <59371877+Kurianska@users.noreply.github.com>
2021-03-13 19:55:35 +0200

    Update day-39.js

--- day-39.js
+++ day-39.js
@@ -1,13 +1,14 @@
 // https://www.codewars.com/kata/5c743cec901022438549964a
 
-const createIterator = (array) => ({
-  index: 0,
-  next() {
-    if (this.index < array.length) {
-      return { value: array[this.index++], done: false };      
-    }
-    else {
-      return { value: undefined, done: true };
-    }
-  }
-});
+function createIterator(array) {
+  const iterator = {
+    index: 0,
+    next: function () {
+      if (iterator.index < array.length) {
+        return { value: array[iterator.index++], done: false };
+      }
+      return { value: array[iterator.index], done: true };
+    },
+  };
+  return iterator;
+}
