6bd61d13e4c6ac2842859f24a7f06930e7e55577
Kurianska <59371877+Kurianska@users.noreply.github.com>
2021-03-13 20:15:54 +0200

    Update day-39.js

--- day-39.js
+++ day-39.js
@@ -3,7 +3,7 @@
 function createIterator(array) {
   const iterator = {
     index: 0,
-    next: function () {
+    next() {
       if (iterator.index < array.length) {
         return { value: array[iterator.index++], done: false };
       }
