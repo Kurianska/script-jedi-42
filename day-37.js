b64f27145338dfc8d93c9991b9380b50dfb637a8
Kurianska <59371877+Kurianska@users.noreply.github.com>
2021-03-13 19:36:20 +0200

    Update day-37.js

--- day-37.js
+++ day-37.js
@@ -1,1 +1,12 @@
+// https://www.codewars.com/kata/587593285448632b8d000143
 
+async function submitOrder(user) {
+  let shoppingCart, zipCode, shippingRate, orderSuccessful;
+  shoppingCart = await OrderAPI.getShoppingCartAsync(user);
+  zipCode = (await CustomerAPI.getProfileAsync(user)).zipCode;
+  shippingRate = calculateShipping(shoppingCart, zipCode);
+  orderSuccessful = await OrderAPI.placeOrderAsync(shoppingCart, shippingRate);
+  console.log(
+    `Your order ${orderSuccessful ? "was" : "was NOT"} placed successfully`
+  );
+}
