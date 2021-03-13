3fd79650308669bf414547e6b32eeded44f0af5b
Kurianska <59371877+Kurianska@users.noreply.github.com>
2021-03-13 19:20:03 +0200

    Update day-41.js

--- day-41.js
+++ day-41.js
@@ -1,1 +1,10 @@
+// https://www.codewars.com/kata/basics-generators-number-1/javascript
 
+
+
+// https://www.codewars.com/kata/5637ead70013386e30000027/
+
+function* generator(a) {
+  const b = 1;
+  while(true) yield `${a} x ${b} = ${a*b++}`
+}
