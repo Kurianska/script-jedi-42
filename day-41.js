d9f759aa43413a594d426d16ecf009e283045fff
Kurianska <59371877+Kurianska@users.noreply.github.com>
2021-03-15 19:09:21 +0200

    Update day-41.js

--- day-41.js
+++ day-41.js
@@ -1,6 +1,12 @@
 // https://www.codewars.com/kata/basics-generators-number-1/javascript
 
-
+function* generator(n) {
+  const count = 1;
+  while (true) {
+    yield count++;
+  }
+  return yield count;
+}
 
 // https://www.codewars.com/kata/5637ead70013386e30000027/
 
