632f17cc3ba39318c057ab71107adfd033991ca7
Kurianska <59371877+Kurianska@users.noreply.github.com>
2021-03-15 16:02:03 +0200

    Update day-42.js

--- day-42.js
+++ day-42.js
@@ -2,7 +2,7 @@
 
 const twoSum = function (nums, target) {
   for (let i = 0; i < nums.length; i++) {
-    for (var j = i + 1; j < nums.length; j++) {
+    for (let j = i + 1; j < nums.length; j++) {
       if (nums[j] == target - nums[i]) {
         return [i, j];
       }
